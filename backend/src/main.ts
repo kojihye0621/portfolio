64545e9b25ca650a308be2e277e12c3336b63ab3
Kojihye <80028726+ycs-202007072@users.noreply.github.com>
2026-07-16 00:36:22 +0900

    backend : 3001 / frontend : 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001); //프론트가 3000 백엔드 3001
 }
 bootstrap();
